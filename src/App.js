7f0ebeb7d725ddaa302887512edaa28c37647a5d
Rozalina <59352861+rozalinaag@users.noreply.github.com>
2022-08-13 15:30:41 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import CreateUser from "./components/Users/CreateUser";
+import CreateUser from './components/Users/CreateUser';
+import UserList from './components/Users/UserList';
+import React, { useState } from 'react';
 
 
 const App = () => {
+  const [userList, setUserList] = useState([]);
 
-  return <div>
-   <CreateUser/>
-  </div>;
+
+  return (
+    <div>
+      <CreateUser onCreateUser = {(name, age) => { 
+        setUserList((prevUserList) => {
+          return [ ...prevUserList, {name: name, age: age, id: Math.random().toString()}];
+        })
+      }}/>
+      <UserList users = {userList}/>
+    </div>
+  );
 };
 
 export default App;
