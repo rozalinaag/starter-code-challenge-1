c2944ef110e88d6eb9f61066f16320148ec1dbca
Rozalina <59352861+rozalinaag@users.noreply.github.com>
2022-08-13 15:31:34 +0300

    Add files via upload

--- src/components/Users/CreateUser.js
+++ src/components/Users/CreateUser.js
@@ -1,22 +1,54 @@
 import styles from './CreateUser.module.css';
 import Card from '../UI/Card';
-
+import Button from '../UI/Button';
+import React, {useState} from 'react';
+import ErrorModal from '../UI/ErrorModal';
 
 const CreateUser = (props) =>{
 
+  const [inputName, setInputName] = useState('');
+  const [inputAge, setInputAge] = useState('');
+  const [error, setError] = useState();
+
   const CreateUserHandler = (event) => {
     event.preventDefault();
+
+    if (inputName.trim().length === 0 || inputAge.trim().length === 0){
+      setError({
+        title: 'Invalid input name', 
+        message: 'These field can`t be empty',
+      })
+      return;
+    }
+    if (inputAge <1){
+      setError({
+        title: 'Invalid input age', 
+        message: 'These field can`t be less then zero',
+      })
+      return;
+    }
+    
+    props.onCreateUser(inputName, inputAge)
+    setError(undefined);
+    setInputAge("");
+    setInputName("");
+  }
+
+  const errorHandler = () => {
+    setError(false);
   }
 
   return (
     <div>
+      {error && <ErrorModal onCloseModal={errorHandler} title={error.title} message={error.message}/> }
+      
       <Card className={styles.input}>
         <form onSubmit={CreateUserHandler}>
           <label htmlFor="name">Name</label>
-          <input  id= "name" type="text"/>
+          <input  id= "name" type="text" value={inputName} onChange={(event) => setInputName(event.target.value)}/>
           <label htmlFor="age">Age</label>
-          <input  id= "age" type="text"/>
-          <button type="submit">Add User</button>
+          <input  id= "age" type="text" value={inputAge} onChange={(event) => setInputAge(event.target.value)}/>
+          <Button type="submit">Add User</Button>
         </form>
       </Card>
     </div>)
